d8d3b733507eb7cfad65a9d74274f47513983ab3
jbdesigns88 <jbdesigns88@gmail.com>
2023-11-10 18:36:27 -0500

    article component added

--- src/components/Article/ArticleComponent.tsx
+++ src/components/Article/ArticleComponent.tsx
@@ -1,8 +1,19 @@
 import React from "react"
+import Grid from "@mui/material/Grid";
+import { useParams } from "react-router-dom";
+import ArticleProvider from "../../Providers/ArticleProvider";
+import { FetchMode } from "../../types/ArticleFetchMode";
 
 const ArticleComponent = () =>  {
+    const{article_id} = useParams();
     return (
-        <></>
+        <ArticleProvider mode={FetchMode.SINGLE} id={article_id}>
+            <Grid container>
+                <Grid item xs={12}> headline</Grid>
+                <Grid item>image</Grid>
+                <Grid item>content</Grid>
+            </Grid>
+        </ArticleProvider>
     )
 
 }
