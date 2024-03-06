f3ce12b6c3df3ba4bc0180fc8ec0fdefac5e237f
jbdesigns88 <jbdesigns88@gmail.com>
2024-03-06 12:59:45 -0500

    updated profile page

--- src/Pages/EditProfile.tsx
+++ src/Pages/EditProfile.tsx
@@ -1,16 +1,14 @@
-import { useContext, useEffect, useLayoutEffect } from "react"
+import {  useEffect } from "react"
 import { useUser } from "../Contexts/UserContext"
 import { useDataSubmitContext } from "../Contexts/DataSubmitContext";
 import DataSubmissionProvider from "../Providers/DataSubmissionProvider";
 import { HttpMethods } from "../Types/DataTypes";
-import { Grid, TextField, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import ContentContainer from "../Utils/ContentContainer";
-import { ArtistDataType } from "../Types/DataTypes";
 import Artist from "../Models/Users/Artist";
 import IgnFormGenerate from "../Components/IgnFormGenerate";
 import { useFormDataContext } from "../Contexts/FormContext";
 import FormDataProvider from "../Providers/FormDataProvider";
-
 
 const Profile = () => {
     const { user } = useUser();
@@ -19,7 +17,6 @@ const Profile = () => {
     const currentUser = new Artist();
 
     useEffect(() => {
-
         const exclusion = ['id']; // will pull this from model or somewhere else.
         if (user) {
             for (const key in user) {
@@ -29,30 +26,26 @@ const Profile = () => {
                 }
             }
         }
-
-
-        // console.log(`the data updated in profile page ${JSON.stringify(data)}`)
     }, [user, updateFormData])
 
+    const handleSubmit = ()=>{
+        console.log(`submitting the data ${JSON.stringify(data)}`)
+    }
 
     return (
         <>
             <Grid item xs={12}> <Typography variant="h4" sx={{ color: "black" ,paddingBottom:2}}> Edit {user?.name}'s Profile</Typography></Grid>
             <IgnFormGenerate formStructures={currentUser.structure} />
+            <Button sx={{marginTop:3,marginBottom:3}} variant="contained" onClick={handleSubmit}>Update</Button>
 
         </>
     )
 }
 
-
 const EditProfile = () => {
-
-
-
-
     return (
         <FormDataProvider>
-            <DataSubmissionProvider httpMethod={HttpMethods.POST} dataUrl={""}>
+            <DataSubmissionProvider httpMethod={HttpMethods.PUT} dataUrl={""}>
                 <ContentContainer>
                     <Profile />
                 </ContentContainer>
--- src/Providers/FormDataProvider.tsx
+++ src/Providers/FormDataProvider.tsx
@@ -7,13 +7,10 @@ interface FormDataProviderProps {
 const FormDataProvider:React.FC<FormDataProviderProps> = ({children}) => {
     const [data,setData] = useState({})
     useEffect(()=>{
-        console.log(`updated ${JSON.stringify(data)}`)
     },[data])
     
     const updateData = (key:string,value:string) => {
-        console.log(`Now should be updating ${key} with ${value}`)
         setData((currentData) => ({...currentData,[key]:value}))
-        console.log(`the data was updated with ${JSON.stringify(data)}`)
     }
 
     return (
