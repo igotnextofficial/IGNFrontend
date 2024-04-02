3732bae6a16a8815170102a9e8044ab289a664ea
jbdesigns88 <jbdesigns88@gmail.com>
2024-04-02 14:03:47 -0400

    if user is an artist we add a link to find mentor

--- src/components/navigation/AccountSettings.tsx
+++ src/components/navigation/AccountSettings.tsx
@@ -1,84 +1,92 @@
-import React,{useContext} from 'react';
+import React, { useContext } from 'react';
 
-import { Avatar, Box, IconButton , Tooltip, Menu , MenuItem, Typography,Grid } from '@mui/material';
+import { Avatar, Box, IconButton, Tooltip, Menu, MenuItem, Typography, Grid } from '@mui/material';
 import { Link } from 'react-router-dom';
 import User from '../../Models/Users/User';
+import { Roles } from '../../Types/Roles';
 import CreateLink from './CreateLink';
 import TemporaryDrawer from '../../Components/Navigation/LeftDrawer';
 
 import { Settings } from '../../Types/DataTypes';
 import { useUser } from '../../Contexts/UserContext';
 
-const AccountSettings  = () => {
-  const {user} = useUser()
-  const settings:Settings[] = [
-      {
-        title:'Profile',
-        slug:'/profile'
-      }, {title:'Account',slug:'/account'}, {title:'Dashboard',slug:`dashboard/${user?.role}`}, {title:'Logout',slug:'/logout'}];
+const AccountSettings = () => {
+  const { user } = useUser()
+  const settings: Settings[] = [
+    {
+      title: 'Profile',
+      slug: '/profile'
+    }, 
+    { title: 'Account', slug: '/account' }, 
+    { title: 'Dashboard', slug: `dashboard/${user?.role}` }, 
+    { title: 'Logout', slug: '/logout' }];
 
 
-    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
-  
+  if(user?.role ===  Roles.ARTIST){
+    settings.unshift({
+       title: 'Find a mentor', slug: '/mentors/find-a-mentor' 
+    })
+  }
 
-    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorElUser(event.currentTarget);
-    };
-  
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-  
-    const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
-    };
- 
 
-    
-  
-    return (
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+
+
+
+  return (
 
     <>
-     <Box sx={{ flexGrow: 0 }}>
+      <Box sx={{ flexGrow: 0 }}>
 
-            <Tooltip title="Open settings">
-              <Grid container spacing={2} alignItems={"center"}>
-                  <Grid item>
-                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+        <Tooltip title="Open settings">
+          <Grid container spacing={2} alignItems={"center"}>
+            <Grid item>
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt={user?.fullname} src={user?.image} />
               </IconButton>
-                  </Grid>
-                  <Grid item> <Typography>Welcome, {user?.fullname} </Typography></Grid>
-              </Grid>
-      
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-           
-              {settings.map(({title,slug}) => (
-                <MenuItem key={slug} onClick={handleCloseUserMenu}>
-          
-                  <CreateLink title={title} slug={slug} />
-                </MenuItem>
-              ))}
-             
-            </Menu>
-          </Box>
-    </> 
-    )
+            </Grid>
+            <Grid item> <Typography>Welcome, {user?.fullname} </Typography></Grid>
+          </Grid>
+
+        </Tooltip>
+        <Menu
+          sx={{ mt: '45px' }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+
+          {settings.map(({ title, slug }) => (
+            <MenuItem key={slug} onClick={handleCloseUserMenu}>
+
+              <CreateLink title={title} slug={slug} />
+            </MenuItem>
+          ))}
+
+        </Menu>
+      </Box>
+    </>
+  )
 }
 
 export default AccountSettings
