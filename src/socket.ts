3cb6e7135df94b00d5481e05d5eb149042be5e10
jbdesigns88 <jbdesigns88@gmail.com>
2025-08-08 14:01:44 -0400

    updated socket

--- src/socket.ts
+++ src/socket.ts
@@ -2,6 +2,7 @@
 // "undefined" means the URL will be computed from the `window.location` object
 
 import { io } from 'socket.io-client';
-const URL = 'https://fury.igotnext.local';
+
+const URL = process.env.REACT_APP_COMMUNICATIONS_BACKEND || ""
  
 export const socket = io(URL);
